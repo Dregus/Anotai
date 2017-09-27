223f4c2640f088abd8bc070bbd9510c7c69bbbd6
Diego Peleteiro <diegopeleteiro97@gmail.com>
2017-09-26 21:17:33 -0300

    Versao Inicial do App

---
+++ Aplicativo/Anotai/app/src/main/java/com/example/h239079/anotai/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.h239079.anotai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
