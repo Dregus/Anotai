e2790d83b3c4144a06deabf509b530081a2e15b9
Alexandre Leite <alexandreajleite@gmail.com>
2017-09-27 17:12:12 -0300

    ID 1 - Cadastro de Investidor

--- Website/Anotai/Controllers/AcessosController.cs
+++ Website/Anotai/Controllers/AcessosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Anotai.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,7 +20,29 @@ namespace Anotai.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Cadastrar(Usuario u)
+        {
+            using (var ctx = new AnotaiContext())
+            {
+                if (u.Nome != null && u.Sobrenome != null && u.Email != null && u.Telefone != null && u.Senha != null)
+                {
+                    u.TipoUsuario = "I";
+                    ctx.Usuarios.Add(u);
+                    ctx.SaveChanges();
+                    return RedirectToAction("Investimentos", "Investidor");
+                }
+                return View();
+            }
+        }
+
         public ActionResult Entrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Entrar(Usuario u)
         {
             return View();
         }
--- Website/Anotai/Models/Usuario.cs
+++ Website/Anotai/Models/Usuario.cs
@@ -9,12 +9,12 @@ namespace Anotai.Models
     public class Usuario
     {
         [Key]
-        public int    Id         { get; set; }
-        public string Nome       { get; set; }
-        public string Sobrenome  { get; set; }
-        public string Telefone   { get; set; }
-        public string TipoUsario { get; set; }
-        public string Email      { get; set; }
-        public string Senha      { get; set; }
+        public int    Id          { get; set; }
+        public string Nome        { get; set; }
+        public string Sobrenome   { get; set; }
+        public string Telefone    { get; set; }
+        public string TipoUsuario { get; set; }
+        public string Email       { get; set; }
+        public string Senha       { get; set; }
     }
 }
